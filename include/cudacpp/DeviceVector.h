d501226cc98e274d050821bd490548d3bb9baa3e
EYEWAY\michael.g <michael.g@eyeway-vision.com>
2018-03-08 10:23:57 +0200

    initial DeviceVector code

---
+++ include/cudacpp/DeviceVector.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "cuda_runtime.h"
+
+
+namespace cudacpp {
+
+
+template<typename T>
+class DeviceVector {
+	T *_p;
+	explicit DeviceVector(T *p) : _p(p) {}
+	DeviceVector() : _p(nullptr) {}
+
+	// we can add a destructor which will cudaFree the memory
+public:
+	__device__ __inline__ T& operator[](std::size_t i) { return _p[i]; }
+	__device__ __inline__ const T& operator[](std::size_t i) const { return _p[i]; }
+	__device__ __host__ __inline__ operator T* () { return _p; }
+
+	static DeviceVector allocate(std::size_t sz) {
+		DeviceVector res;
+		cudaMalloc((void**)&res._p, sizeof(T) * sz);
+		return res;
+	}
+};
+
+
+}
+
